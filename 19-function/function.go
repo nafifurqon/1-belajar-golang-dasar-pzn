c42fff580b6c3f92642d6a610b7da8ea591fe0bc
Muhammad Nafi' Furqon Diani <muhammad.diani@manuva.com>
2023-02-26 08:08:29 +0700

    19. Function

---
+++ 19-function/function.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func sayHello() {
+	fmt.Println("Hello, world!")
+}
+
+func main() {
+	// sayHello()
+	// sayHello()
+	// sayHello()
+
+	for i := 0; i < 10; i++ {
+		sayHello()
+	}
+}
