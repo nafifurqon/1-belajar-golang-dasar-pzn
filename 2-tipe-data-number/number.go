9ca88e192d2321ce3d330fbdb187f151bc4ee000
Muhammad Nafi' Furqon Diani <muhammadnafi98@gmail.com>
2025-06-24 08:04:31 +0700

    update notes in 2-tipe-data-number

--- 2-tipe-data-number/number.go
+++ 2-tipe-data-number/number.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Satu = ", 1)
+	fmt.Println("Satu = ", 1) // default data type is int
 	fmt.Println("Dua = ", 2)
-	fmt.Println("Tiga Koma Lima = ", 3.5)
+	fmt.Println("Tiga Koma Lima = ", 3.5) // default data type is float
+
+	var test int8 = 100
+	fmt.Println("Test = ", test)
+	// test = 128 => cannot use 128 (untyped int constant) as int8 value in assignment (overflows)
+
+	// byte = uint8
+	// rune = int32
+	// int = int32
+	// uint = uint32
 }
